78a4209a92aac312d7ed8584771e8936f9f63a2d
watcharaphat <watcharaphat.3166@gmail.com>
2018-05-01 23:23:47 +0700

    add title img

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -5,7 +5,7 @@
         <div class="navbar-brand">
           <a class="navbar-item">
             <img
-              src="../../../static/logo/helium-logo.png"
+              src="../../../static/logo/helium-logo-with-name.png"
               alt="Bulma: a modern CSS framework based on Flexbox"
               height="28"
             />
